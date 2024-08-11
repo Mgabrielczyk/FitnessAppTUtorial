057c37faf38a56e3df7bd10914eadb30499147ea
Mgabrielczyk <m.gabrielczyk44@gmail.com>
2024-08-11 16:39:40 +0200

    first commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Michał Gabrielczyk on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
